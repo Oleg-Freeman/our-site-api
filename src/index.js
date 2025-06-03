9984a087afc1da27e160809a7a673d4add274ba8
Oleg Voitiuk <youremail@example.com>
2025-06-03 15:42:33 +0300

    Fix JSON limit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 morgan.token('body', loggerMiddleware);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan(':date[iso] :method :url :status :response-time ms body :body'));
 
